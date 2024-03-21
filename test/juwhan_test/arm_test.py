ba0a4edbfca8a83613e29dff0abdffd104da9054
sunjuwhan <mathsun0011@gmail.com>
2024-03-21 15:44:04 +0900

    Create arm_test.py

---
+++ test/juwhan_test/arm_test.py
@@ -0,0 +1,42 @@
+from dronekit import connect ,VehicleMode,LocationGlobalRelative,APIEXception
+
+import time
+import socket
+import builtins
+import math
+import argparse
+
+
+def connectMyCopter():
+    pasrser= argparse.ArgumentParser(description="commands")
+    pasrser.add_argument("--connect")
+    args=pasrser.parse_args()
+    
+    
+    connection_string=args.connect 
+    baud_rate=57600
+    vehicle=connect(connection_string,baud=baud_rate,wait_ready=True)
+    return vehicle
+
+
+def arm():
+    while vehicle.is_armable==False:
+        print("Wating for vehicle to become armable....")
+        time.sleep(1)
+    print("Toooo vehicle is now armable")
+    print("**")
+    
+    vehicle.armed=True
+    while vehicle.armed==False:
+        print("Wating for drone to become armed")
+        time.sleep(1)
+    
+    
+    print("Vehicle is now armed")
+    print("OMG props ar oping LOOK out")
+    return None
+
+
+vehicle=connectMyCopter()
+arm()
+print("End of scripts ")
